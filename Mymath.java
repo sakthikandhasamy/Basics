b074feb1c62589bee942e163a7aacf8c518f13aa
sakthikandhasamy <142565829+sakthikandhasamy@users.noreply.github.com>
2023-09-04 12:47:07 +0530

    Add files via upload
    Adding files via drag and drop

---
+++ Mymath.java
@@ -0,0 +1,19 @@
+public class Mymath
+{
+public static void main(String args[])
+{
+System.out.println(Math.min(2,7));
+System.out.println(Math.max(1,3));
+System.out.println(Math.sqrt(25));
+System.out.println(Math.cbrt(27));
+System.out.println(Math.sin(90));
+System.out.println(Math.cos(90));
+System.out.println(Math.tan(90));
+System.out.println(Math.log(10));
+System.out.println(Math.log10(1000));
+System.out.println(Math.pow(2,0));
+System.out.println(Math.ceil(7.5));
+System.out.println(Math.floor(7.5));
+System.out.println(Math.round(7.5));
+}
+}
